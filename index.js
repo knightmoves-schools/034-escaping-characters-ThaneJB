3828a7d45f68b0097d908f78a60a3532372ca152
Thane <thabay0606@students.maquoketaschools.org>
2025-10-20 14:30:46 -0500

    Added string variable named introduction, added new line, tab, single quote, double quote, and a backslash to introduction.

---
+++ index.js
@@ -0,0 +1,6 @@
+var introduction = 'Dear Deb,\n' +
+"\tThank you for such a \"wonderful\" visit.\n" +
+'I will remember it \'forever\'.\n\n' +
+'Sincerely,\n' +
+'Travis Scott :\\'
+document.getElementById("result").innerHTML = introduction; 
